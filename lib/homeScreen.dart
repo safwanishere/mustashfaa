ef1941e49d7e49d8fd1e3505889db5ea22e05d54
safwan <mohammedsafwan261205@gmail.com>
2024-01-22 23:19:40 +0530

    home page complete

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -20,12 +20,67 @@ class _homeScreenState extends State<homeScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Padding(
-          padding: EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            children: [
-              SizedBox(height: 20,),
-              Image.asset("assets/homePageTop.png")
-            ],
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 20,),
+                Image.asset("assets/homePageTop.png", width: 700,),
+                const SizedBox(height: 20),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Top Discounts", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("All", style: TextStyle(fontSize: 12))
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset("../assets/apollo.jpg", height: 100,)
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset("../assets/medplus.png", height: 100,)
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset("../assets/medilife.png", height: 100,)
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset("../assets/1mg.png", height: 100,)
+                    )
+                  ],
+                ),
+            
+                const SizedBox(height: 40,),
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: Colors.greenAccent),
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("SALE!", style: TextStyle(color: Color.fromARGB(255, 33, 136, 36), fontWeight: FontWeight.bold, letterSpacing: 15, fontSize: 30)),
+                          SizedBox(height: 20,),
+                          Text("Avail 5% off \non all offers today", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w600, letterSpacing: 2, fontSize: 16),)
+                        ]
+                      ),
+                      Image.asset("../assets/starsBG.png", height: 120,),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 40,),
+              ],
+            ),
           ),
         ),
       ),
