42f72413f6441697fa2e8ffc7d0cf0b6676849e7
safwan <mohammedsafwan261205@gmail.com>
2024-02-23 23:40:07 +0530

    completed account page

--- lib/accountScreen.dart
+++ lib/accountScreen.dart
@@ -13,7 +13,40 @@ class _accountScreenState extends State<accountScreen> {
     return Scaffold(
       body: Container(
         color: const Color.fromRGBO(255, 234, 238, 1),
-        
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20,),
+                Text("Your Profile"),
+                const SizedBox(height: 20,),
+                Container(height: 2, width: MediaQuery.of(context).size.width, color: Color.fromARGB(255, 182, 182, 182),),
+                const SizedBox(height: 20,),
+                Text("Your Cart"),
+                const SizedBox(height: 20,),
+                Container(height: 2, width: MediaQuery.of(context).size.width, color: Color.fromARGB(255, 182, 182, 182),),
+                const SizedBox(height: 20,),
+                Text("Wishlists"),
+                const SizedBox(height: 20,),
+                Container(height: 2, width: MediaQuery.of(context).size.width, color: Color.fromARGB(255, 182, 182, 182),),
+                const SizedBox(height: 20,),
+                Text("Upcoming Appointments"),
+                const SizedBox(height: 20,),
+                Container(height: 2, width: MediaQuery.of(context).size.width, color: Color.fromARGB(255, 182, 182, 182),),
+                const SizedBox(height: 20,),
+                Text("Previous Orders"),
+                const SizedBox(height: 20,),
+                Container(height: 2, width: MediaQuery.of(context).size.width, color: Color.fromARGB(255, 182, 182, 182),),
+                const SizedBox(height: 20,),
+                Text("Log out", style: TextStyle(color: Color.fromRGBO(218, 44, 56, 1)),),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
